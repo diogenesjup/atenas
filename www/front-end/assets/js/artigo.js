8f9e27e22684dc6a737bcbdb06c83770717d2cd9
Diogenes Junior <diogenesjunior@Mac-mini-de-Diogenes.local>
2021-10-24 11:10:56 -0300

    correção do nome do app

--- www/front-end/assets/js/artigo.js
+++
@@ -1,6 +0,0 @@
-// AUMENTA FONTE ARTIGO
-const plusText = document.querySelector("[data-js='plus']");
-plusText.addEventListener("click", () => {
-  const content = document.querySelector(".artigo__content");
-  content.classList.toggle("plus");
-});
--- www/front-end/assets/js/post.js
+++
@@ -1,4 +0,0 @@
-const btn = document.querySelector("[data-js='preview']");
-btn.addEventListener("click", () => {
-    document.querySelector(".modal").style.display = "flex";
-});
